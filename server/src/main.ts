2868d6daf412483940c94024098b5c110eec4a83
Marc Alexandre <github@malexandre.fr>
2023-06-13 16:03:25 +0200

    feat(server): Instantiate server

---
+++ server/src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    methods: ['GET', 'POST'],
+    origin: '*',
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  await app.listen(3000);
+}
+bootstrap();
